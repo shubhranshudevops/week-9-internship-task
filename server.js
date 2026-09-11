0a821531326ad9f52fa1a24bcd813deb73a4e6eb
shubhranshudevops <shubhranshudevops@gmail.com>
2026-09-11 16:27:51 +0530

    Update success message for Jenkins CI/CD Pipeline

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Jenkins CI/CD Pipeline is working successfully!");
+    res.send("Jenkins CI/CD Pipeline VERSION 1 is working successfully!");
 });
 
 app.get("/health", (req, res) => {
